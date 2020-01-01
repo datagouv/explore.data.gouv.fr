2567ce4ee763a17492fc6ef8249a05cb2a1b8121
Alexandre Bulté <alexandre@bulte.net>
2020-01-01 22:11:48 +0100

    bootstrap filter form

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <b-form inline>
+      <!-- TODO: make this a [] of components -->
+      <b-form-select value-field="key" text-field="label" v-model="filterField" :options="fields"></b-form-select>
+      <b-form-select v-model="filterComp" :options="comparators"></b-form-select>
+      <b-input v-model="filterValue"></b-input>
+      <b-button variant="primary">Filtrer</b-button>
+    </b-form>
     <b-table hover small :items="rows" :fields="fields" :no-local-sorting="true" @sort-changed="sort"></b-table>
     <b-pagination align="center" size="lg" :total-rows="totalRows" v-model="page" :per-page="pageSize" @input="changePage"></b-pagination>
   </div>
@@ -10,6 +17,17 @@ import {pageSize} from '@/config'
 
 export default {
   'name': 'Table',
+  data () {
+    return {
+      comparators: [
+        {value: '', text: 'est égal à'},
+        {value: 'contains', text: 'contient'}
+      ],
+      filterField: '',
+      filterComp: '',
+      filterValue: ''
+    }
+  },
   computed: {
     rows () {
       return this.$store.state.rows
