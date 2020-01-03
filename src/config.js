bb2088c2870a4a0d8c255e4d235b4c684f1bce8c
Alexandre Bulté <alexandre.bulte@data.gouv.fr>
2020-01-03 15:07:18 +0100

    Add filters support (#7)

--- src/config.js
+++ src/config.js
@@ -2,16 +2,24 @@
  * Simple helper to fetch attribute on given css selector
  */
 function _attr(selector, name) {
-    const el = document.querySelector(selector);
-    return el ? el.getAttribute(name) : undefined;
+  const el = document.querySelector(selector)
+  return el ? el.getAttribute(name) : undefined
 }
 
 /**
  * Simple helper to <meta/> tag content given its name
  */
 function _meta(name) {
-    return _attr(`meta[name=${name}]`, 'content');
+  return _attr(`meta[name=${name}]`, 'content')
 }
 
-export const csvapiUrl = process.env.VUE_APP_CSVAPI_URL || _meta('csvapi-url');
-export const pageSize = _meta('page-size') || 10;
+export const csvapiUrl = process.env.VUE_APP_CSVAPI_URL || _meta('csvapi-url')
+export const pageSize = _meta('page-size') || 10
+
+let _filtersEnabled = process.env.VUE_APP_FILTERS_ENABLED || _meta('filters-enabled')
+try {
+  _filtersEnabled = JSON.parse(_filtersEnabled)
+} catch {
+  _filtersEnabled = false
+}
+export const filtersEnabled = _filtersEnabled
