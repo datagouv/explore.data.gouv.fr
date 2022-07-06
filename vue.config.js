8423ad7769e3474ac630408d75c836044d9ce3b7
quaxsze <plum.stanislas@gmail.com>
2022-07-06 11:47:24 +0200

    0.6.2

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,15 @@ module.exports = {
       if(config.plugins.has('extract-css')) {
         const extractCSSPlugin = config.plugin('extract-css')
         extractCSSPlugin && extractCSSPlugin.tap(() => [{
-          filename: 'css/[name].css'
+          filename: 'css/[name].css',
+          chunkFilename: 'css/[name].css'
         }])
       }
     },
     configureWebpack: {
       output: {
-        filename: 'js/[name].js'
+        filename: 'js/[name].js',
+        chunkFilename: 'js/[name].js'
       }
     }
   }
