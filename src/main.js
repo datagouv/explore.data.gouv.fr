02342f7eed97acee0b903986478305969ffcc92b
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-08-16 14:27:04 +0200

    perf: :fire: remove unused dependencies

--- src/main.js
+++ src/main.js
@@ -6,16 +6,11 @@ import 'vue-loading-overlay/dist/vue-loading.min.css'
 
 import VueResource from 'vue-resource'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import App from './App.vue'
 import store from './store'
 
 Vue.use(Loading)
 Vue.use(VueResource)
-Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
