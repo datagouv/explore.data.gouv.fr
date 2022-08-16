312d0a8f2afe4440441aefe09bf10595f5c4a7e9
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-08-16 11:56:35 +0200

    feat: :sparkles: use new tooltip

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,62 @@
+<template>
+    <div 
+    class="fr-p-2w relCell relSiren" 
+    >
+    <p class="fr-text--xs relCell-explanation">
+        <span class="fr-fi--sm fr-icon-lightbulb-line" aria-hidden="true"></span>
+        {{ explanation }}
+    </p>
+    <p class="fr-text--bold fr-m-0">
+        {{ content }}
+    </p>
+    <a 
+        class="fr-text--sm"
+        :href="linkHref"
+        target="_blank"
+    >
+        {{ link }}
+    </a>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        explanation: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        link: {
+            type: String,
+        },
+        linkHref: {
+            type: String,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.relCell-explanation {
+    color: var(--text-mention-grey);
+    font-style: italic;
+}
+
+.relCell {
+    display: none;
+    z-index: 5;
+    position: absolute;
+    bottom: -80px;
+    left: 4px;
+    width: 450px;
+    background-color: white;
+    font-weight: normal;
+    font-size: 13px;
+    box-shadow: 0px 1px 4px 0px #0000003B,
+                0px 1px 0px -2px #00000029,
+                0px 1px 1px 0px #00000029;
+}
+</style>
