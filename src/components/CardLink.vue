52268a8d51b09e2b66a5170cae55bd000d0732ca
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2022-09-13 10:49:56 +0200

    Add card in home to go directly to specific exploration

---
+++ src/components/CardLink.vue
@@ -0,0 +1,88 @@
+<template>
+    <div
+        @click="goToExplore()" 
+        class="fr-container fr-mt-2w card"
+    >
+        <div class="fr-card fr-row">
+            <div class="fr-card__body cardDetail">
+                <div class="logoCard">
+                    <img :src="logo" width="100"/>
+                </div>
+                <div class="titleCard">
+                    <h3 class="fr-card__title">
+                        {{ dataset.title }}
+                    </h3>
+                    <p class="fr-card__desc">
+                        {{ resource.title }}
+                    </p>
+                    <br />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'CardLink',
+    props: ['did', 'rid'],
+    data() {
+      return {
+        dataset: {},
+        resource: {},
+        logo: '',
+      }
+    },
+    methods: {
+        goToExplore(){
+            window.location.href = window.location.origin + '/?url=https://www.data.gouv.fr/fr/datasets/r/' + this.rid
+        }
+    },
+    async created() {
+        console.log(this.did)
+        this.dataset = await fetch('https://www.data.gouv.fr/api/1/datasets/' + this.did)
+        .then((response) => {
+            return response.json()
+        })
+        this.resource = await fetch('https://www.data.gouv.fr/api/2/datasets/resources/' + this.rid)
+        .then((response) => {
+            //console.log(response.json())
+            return response.json()
+        })
+        this.resource = this.resource['resource']
+        console.log(this.dataset.organization)
+        if(this.dataset.owner) {
+            this.logo = this.dataset.owner.avatar
+        } else {
+            this.logo = this.dataset.organization.logo
+        }
+    },
+}
+</script>
+
+<style>
+
+.card{
+    cursor: pointer
+}
+
+.cardDetail{
+    width: 100%;
+    display: -webkit-box;
+    padding: 10px;
+}
+
+.logoCard{
+    line-height: 100%;
+    margin-top: auto;
+    margin-bottom: auto;
+}
+
+.titleCard{
+    margin-left: 20px;
+    margin-top: auto;
+    margin-bottom: auto;
+}
+
+</style>
--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -3,16 +3,16 @@
   <div class="fr-mt-4w fr-container" v-if="!csvUrl">
     <h2>Bienvenue sur le prototype d'exploration des données de data.gouv.fr</h2>
     <p>Ce prototype vise à permettre d’explorer plus facilement les données référencées sur data.gouv.fr.<br />
-      Sélectionnez un fichier qui vous intéresse sur data.gouv.fr et collez le lien dans la barre ci-dessous pour l’explorer.</p>
-    <div class="fr-callout">
+      Sélectionnez un fichier de moins de 100Mo qui vous intéresse sur data.gouv.fr et collez le lien dans la barre ci-dessous pour l’explorer.</p>
+    <!-- <div class="fr-callout">
       <h3 class="fr-callout__title">Précautions d'usages</h3>
       <p class="fr-callout__text">
         Si l'explorateur est utilisé sur un jeu de données pour la première fois, le chargement peut prendre un certain temps.
         Ce prototype ne permet pas d’explorer les fichiers de plus de 100 Mo.
       </p>
-    </div>
+    </div> -->
     <form class="fr-mt-4w">
-      <label class="fr-label" for="text-input-text">URL du fichier à visualiser au format CSV ou XLS
+      <label class="fr-label" for="text-input-text">URL du fichier à visualiser au format CSV
         <span class="fr-hint-text">Il s’agit du lien vers un fichier et non d’une page de jeu de données</span>
       </label>
       <input class="fr-input fr-mb-2w" type="text" v-model="csvUrlFieldValue" id="text-input-text" name="text-input-text" />
@@ -22,6 +22,9 @@
         </button>
       </div>
     </form>
+    <br /><br />
+    <p>Si vous ne savez pas par quoi commencer à explorer, nous vous proposons ci-dessous une sélection de quelques jeux de données.</p>
+    <CardLink v-for="item in listResources" :key="item.resource_id" :did="item.dataset_id" :rid="item.resource_id"></CardLink>
   </div>
   <!-- error block -->
   <Error v-else-if="hasError" :error="error"></Error>
@@ -35,14 +38,25 @@
 import Table from '@/components/Table'
 import Error from '@/components/Error'
 import Loader from '@/components/Loader'
+import CardLink from '@/components/CardLink'
 
 export default {
   name: 'TableView',
-  components: {Table, Error, Loader},
+  components: {Table, Error, Loader, CardLink},
   data() {
     return {
       csvUrl: '',
-      csvUrlFieldValue: ''
+      csvUrlFieldValue: '',
+      listResources: [
+        {
+          'dataset_id': '5448d3e0c751df01f85d0572',
+          'resource_id': '8d9398ae-3037-48b2-be19-412c24561fbb',
+        },
+        {
+          'dataset_id': '6311c164ebfb165ddc828ded',
+          'resource_id': '1c5075ec-7ce1-49cb-ab89-94f507812daf',
+        },
+      ]
     }
   },
   computed: {
