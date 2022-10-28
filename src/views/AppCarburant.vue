d068caa79bda40a0b4fc526069d25e1a46b4002a
Geoffrey Aldebert <geoffrey.aldebert@data.gouv.fr>
2022-10-28 09:48:37 +0200

    docs: add article dgv

--- src/views/AppCarburant.vue
+++ src/views/AppCarburant.vue
@@ -264,16 +264,13 @@
                           {{ this.legend.medianPrix }} €
                         </div>
                       </div>
-                      <div v-if="this.legend.percentRupture" class="fr-my-5v">
-                        <div class="fr-text--bold fr-mb-1v">Stations en rupture de {{ currentFuel }}* :</div>
-                          {{ this.legend.percentRupture }} %
-                      </div>
+                      <br />
                       <div v-if="valuesx && valuesy">
                         <bar-or-graph indicateur="toto" color="#3558a2" :titleChart="titleChart" unitChart="€" typeChart="line" :valuesx="valuesx" :valuesy="valuesy"></bar-or-graph>
                       </div>
                       <br />
                       <div class="nb-legend">
-                        <i>* Les ruptures ne sont comptabilisées qu'à partir du 15 septembre 2022</i>
+                        <i><u><a href="https://www.data.gouv.fr/fr/posts/exploration-de-donnees-zoom-sur-de-nouvelles-briques-disponibles-sur-data-gouv-fr-avec-lexemple-du-prix-des-carburants/">En savoir plus sur ce tableau de bord</a></u> et la méthodologie permettant son développement.</i>
                         <br />
                         <i>Les sources de données utilisées pour réaliser cette application <a href="https://www.data.gouv.fr/fr/datasets/prix-des-carburants-en-france-flux-instantane/"><u>sont disponibles sur data.gouv.fr</u></a> et <a href="https://explore.data.gouv.fr/?url=https://www.data.gouv.fr/fr/datasets/r/64e02cff-9e53-4cb2-adfd-5fcc88b2dc09"><u>sont explorables ici.</u></a></i>
                         <i> Celles-ci sont mises à disposition par le Ministère de l'Économie, des Finances et de la Souveraineté industrielle et numérique.
