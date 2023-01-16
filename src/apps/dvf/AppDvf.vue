aa2bd97b940bceab227af7d5629d3645cf481e10
Nicolas Boeuf <nicolas_boeuf@hotmail.fr>
2023-01-16 14:36:59 +0100

    add dvf app repo

---
+++ src/apps/dvf/AppDvf.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="fr-container--fluid">
+    <header-apps></header-apps>
+    <h1>DVF</h1>
+  </div>
+</template>
+
+<script>
+
+import store from './store'
+
+import HeaderApps from '@/views/HeaderApps'
+
+export default {
+  name: 'AppDvf',
+  components: {HeaderApps},
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  mounted() {
+  },
+  methods: {
+  },
+  watch: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
