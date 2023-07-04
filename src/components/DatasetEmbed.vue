011ce951c9541be2b86947f591e662617d40e0fa
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2023-07-04 23:59:17 +0200

    feat: many features

--- src/components/DatasetEmbed.vue
+++ src/components/DatasetEmbed.vue
@@ -1,30 +1,30 @@
 <template>
   <div>
-    <div data-udata-dataset="63bce30c70d6d100b09dde9a"></div><component is="script" data-udata="https://www.data.gouv.fr/" src="https://static.data.gouv.fr/static/oembed.js" async defer></component>
-   </div>
+    <div :data-udata-dataset="dataset"></div>
+    <component
+      is="script"
+      data-udata="https://www.data.gouv.fr/"
+      src="https://static.data.gouv.fr/static/oembed.js"
+      async
+      defer
+    ></component>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'DatasetEmbed',
-  data(){
-    return {}
+  name: "DatasetEmbed",
+  data() {
+    return {};
   },
   props: {
-    dataset:String
+    dataset: String,
   },
-  computed: {
-  },
-  methods: {
-    
-  },
-  created(){
-    
-  }
-}
+  computed: {},
+  methods: {},
+  created() {},
+};
 </script>
 
 <style scoped lang="scss">
-
 </style>
